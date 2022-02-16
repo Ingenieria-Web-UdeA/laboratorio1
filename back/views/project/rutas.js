a7c8b94b1188828630924886c1425ef88126bfb0
Esteban Restrepo <esteban.restrepo@amadeus.com>
2022-02-16 17:49:32 -0500

    api get all projects only for admin

--- back/views/project/rutas.js
+++ back/views/project/rutas.js
@@ -8,6 +8,26 @@ const prisma = new PrismaClient();
 
 const rutasProject = express.Router();
 
+rutasProject.route('/project').get(async (req, res) => {
+  const {proyecto} = req.body;
+  const currUser = auth.isAuth(req.headers.user);
+  currUser.then(async (loggedUser)=>{
+    if (loggedUser && loggedUser.role === 'Administrador') {
+      try {    
+        const proyectos = await prisma.project.findMany();
+    
+        res.status(201).send({ status: 'ok', proyectos});
+      } catch {
+        res.status(500).send({ status: 'error' });
+      }
+    }else{ 
+      res.status(401).send({ status: 'error', message: 'No tiene los permisos necesarios para realizar la operacion'});
+    }
+  }).catch((err) => {
+    res.status(404).send({ status: 'error' });
+  })
+});
+
 rutasProject.route('/project').post(async (req, res) => {
   const {proyecto} = req.body;
   const currUser = auth.isAuth(req.headers.user);
