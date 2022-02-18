6a03a187bff57235ad637aa42171e4119c6c6101
Santiago <santiagomarinocampo@gmail.com>
2022-02-18 10:48:53 -0500

    Detalles proyecto finalizado vista

--- front/src/pages/proyecto/DetallesProyecto.jsx
+++ front/src/pages/proyecto/DetallesProyecto.jsx
@@ -7,7 +7,8 @@ import axios from 'axios';
 import ItemIssue from 'components/ItemIssue';
 import Issues from 'pages/issue/Issues';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 
@@ -19,7 +20,6 @@ const DetallesProyecto = () => {
     const [empresa, setEmpresa] = useState('');
     const [descripción, setDescripción] = useState('');
     const [issues, setIssue] = useState([]);
-
     const getAllissue = async () => {
         const options = {
             method: 'GET',
@@ -31,6 +31,7 @@ const DetallesProyecto = () => {
         const respuesta = await axios.request(options);
         setIssue(respuesta.data.issues);
     };
+
 
 
     const getbyidProyecto = async () => {
@@ -55,23 +56,25 @@ const DetallesProyecto = () => {
             status: 30,
         }])
         setNombre('Proyecto1')
+        setDescripción('Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.')
+        setEmpresa('Empresa1')
     }, [])
 
     return (
-        <div className='flex flex-col w-full'>
-            <div className='flex justify-center'>
-                    <h1 className='text-4xl font-bold text-gray-800 my-10  font-sans'>
-                        {nombre}
-                    </h1>
-                    <p>
-                        {descripción}
-                    </p>
-                
+        <div className='flex flex-col w-full items-center'>
+            <h1 className='text-4xl font-bold my-10  font-sans'>
+                {nombre}
+            </h1>
+            <div className='flex flex-col justify-center gap-6 mx-96'>
+                <span className='text-lg text-center'>{empresa}</span>
+                <p className='block p-3 text-lg rounded mb-4 '>
+                    {descripción}
+                </p>
             </div>
 
-            <div className='flex flex-col pt-8 px-16  w-full'>
-                <div className='flex justify-between w-full items-center'>
-                    <span className='block text-2xl font-bold'>Issues</span>
+            <div className='flex flex-col pt-16 px-16 w-full'>
+                <div className='flex justify-between items-center px-11'>
+                    <span className='block text-4xl font-bold'>Issues</span>
                     <div className='flex flex-row items-center gap-4'>
                         <label
                             className='block text-gray-700 text-xl font-bold'
@@ -88,8 +91,8 @@ const DetallesProyecto = () => {
                         />
                     </div>
                 </div>
-                <div className='flex flex-col w-full gap-[2px] px-2 py-10'>
-                    <div className=' flex flex-row items-center w-full h-[50px] px-3 justify-between'>
+                <div className='flex flex-col gap-[2px] px-2 py-10'>
+                    <div className=' flex flex-row items-center h-[50px] px-3 justify-between'>
                         <span>Nombre de Issue</span>
                         <span className='fixed left-[500px]'>Proyecto</span>
                         <div className='flex flex-row justify-between items-center w-[1000px] pr-20'>
